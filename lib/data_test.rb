b984a26f3b7aecbef59fa76e7694d91a4acb850b
Alex Norton <sandig@gmail.com>
2014-03-06 23:40:35 +0000

    :)

--- lib/data_test.rb
+++ lib/data_test.rb
@@ -6,7 +6,7 @@ require_relative "db_helpers"
 require_relative "geo_helpers"
 
 # Any tests for data routes can go here
-Requests.get_venues_of_type_in_range("54.0056373,-2.7849923", 500, "coffee")
+#Requests.get_venues_of_type_in_range("54.0056373,-2.7849923", 500, "coffee")
 
 DBHelpers.insert_travel_time_data(Requests.get_travel_time_data(nil))
 
