99ba3fa62b1d03931a68ca8708688cddc46ae95a
Alex Norton <sandig@gmail.com>
2014-03-06 13:50:37 +0000

    Initial database schema

---
+++ model/sql/db.sql
@@ -0,0 +1,51 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4096
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: 127.0.0.1 (MySQL 5.6.15)
+# Database: wonder
+# Generation Time: 2014-03-06 13:50:08 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table journey_times
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `journey_times`;
+
+CREATE TABLE `journey_times` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `timestamp` datetime DEFAULT NULL,
+  `recorded_timestamp` datetime DEFAULT NULL,
+  `point` point DEFAULT NULL,
+  `from_point` point DEFAULT NULL,
+  `to_point` point DEFAULT NULL,
+  `ideal_time` int(11) DEFAULT NULL,
+  `historic_time` int(11) DEFAULT NULL,
+  `estimated_time` int(11) DEFAULT NULL,
+  `name` int(11) DEFAULT NULL,
+  `direction` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
