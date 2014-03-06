8605913266f8a27c0bd30ff1594dbb29bbba12a8
Charlie Revett <charlierevett@gmail.com>
2014-03-06 16:10:20 +0000

    Added animation to leaflet map

--- public/js/function.js
+++ public/js/function.js
@@ -1,4 +1,14 @@
 $(function() {
+  /******
+  GLOBALS
+  ******/
+
+  var map;
+
+  /*******************
+  LOCATION AND MAPPING
+  *******************/
+
   // Use HTML5 GeoLocation to get lat/long
   var getLocation = function() {
     if (navigator.geolocation) {
@@ -43,13 +53,39 @@ $(function() {
 
   // Create Leaflet Map centered at user's location
   var initLeafletMap = function(lat, lon) {
-    var map = L.map('map').setView([lat, lon], 14);
+    map = L.map('map').setView([lat, lon], 14);
 
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 18
     }).addTo(map);
-    L.marker([lat, lon]).addTo(map);
+
+    L.marker([lat, lon], { 
+      bounceOnAdd: true
+    }).addTo(map);
+
+    $('#map_btn').show();
   };
 
   getLocation();
+
+  /********
+  LISTENERS
+  ********/
+
+  // Animate expanding the map
+  $('#map_btn').click(function(){
+    var height = $('#map').height();
+
+    if (height > 180) {
+      $('#map').animate({height:180},200);
+      $('#map_btn_wrapper').animate({"margin-top":170},200);
+      $('#map_btn').addClass('glyphicon-chevron-down');
+      $('#map_btn').removeClass('glyphicon-chevron-up');
+    } else {
+      $('#map').animate({height:360},200);
+      $('#map_btn_wrapper').animate({"margin-top":350},200);
+      $('#map_btn').addClass('glyphicon-chevron-up');
+      $('#map_btn').removeClass('glyphicon-chevron-down');
+    }
+  });
 });
