7cb8d8add253e99fd89ce63504c854f681574f3a
Alex Norton <sandig@gmail.com>
2014-03-06 15:49:59 +0000

    Importing records in bulk

--- lib/db_helpers.rb
+++ lib/db_helpers.rb
@@ -1,5 +1,6 @@
 require_relative '../model/init'
 require 'active_support/all'
+require 'activerecord-import'
 
 class DBHelpers
   def initialize(args)
@@ -9,9 +10,11 @@ class DBHelpers
   def self.insert_travel_time_data(data)
     JourneyTime.destroy_all
 
+    records = []
+
     data.each do |record|
       
-      JourneyTime.create(
+      records << JourneyTime.new(
           id: record['id'],
           timestamp: record['timestamp_readable'].to_datetime,
           recorded_timestamp: record['recordedtime'].to_datetime,
@@ -25,6 +28,8 @@ class DBHelpers
           direction: record['direction']
         )
     end
+
+    JourneyTime.import records
   end
 end
 
