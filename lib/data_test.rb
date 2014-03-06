c011c16dece397c13d71a80077a4a478e6e7a6f5
Chris Foxley-Evans <cfoxleyevans.compsci@gmail.com>
2014-03-06 23:40:00 +0000

    weather data

--- lib/data_test.rb
+++ lib/data_test.rb
@@ -8,7 +8,7 @@ require_relative "geo_helpers"
 # Any tests for data routes can go here
 # Requests.get_venues_of_type_in_range("54.0056373,-2.7849923", 500, "coffee")
 
-# DBHelpers.insert_travel_time_data(Requests.get_travel_time_data(nil))
+DBHelpers.insert_travel_time_data(Requests.get_travel_time_data(nil))
 
 p results = DBHelpers.get_intersecting_journey_time_records(54.0056373, -2.7849923, 10)
 
--- routes/weather.rb
+++ routes/weather.rb
@@ -14,4 +14,17 @@ class WonderApp < Sinatra::Base
 
 		response.body
 	end
+
+	get '/weather/nearby_weather' do
+		content_type :json
+		
+		uri =  URI.parse("https://api.forecast.io/forecast/89e98468f9817b18cd7f337bd4256982/#{params['lat']},#{params['long']}?units=uk")
+
+		http = Net::HTTP.new(uri.host, uri.port)
+		http.use_ssl = true
+		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+		response = http.get(uri.request_uri)
+
+		JSON.parse(response.body)
+	end
 end
