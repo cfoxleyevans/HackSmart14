b0b23be0f4a366b9d2780dabde9180d4e229ebb5
Charlie Revett <charlierevett@gmail.com>
2014-03-07 03:04:48 +0000

    Added PREDICTION!!!!!!!!!

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -6,14 +6,27 @@ module Helpers
 
   # Helper Methods for Prediction Route
 
-  def number_of_road_works lat, long, radius
+  def road_works_impact lat, long, radius
     bounding_box_coords = GeoHelpers.get_bounding_coords(lat, long, radius)
 
-    Roadwork.find_by_sql("SELECT * FROM `roadworks` WHERE MBRContains(GeomFromText('POLYGON((#{bounding_box_coords[:top_right][1]} #{bounding_box_coords[:top_right][0]}, #{bounding_box_coords[:top_left][1]} #{bounding_box_coords[:top_left][0]}, #{bounding_box_coords[:bottom_left][1]} #{bounding_box_coords[:bottom_left][0]}, #{bounding_box_coords[:bottom_right][1]} #{bounding_box_coords[:bottom_right][0]}, #{bounding_box_coords[:top_right][1]} #{bounding_box_coords[:top_right][0]}))'),
-      POINT(point)) and id in (select max(id) from roadworks group by point);")
+    rws = Roadwork.find_by_sql("SELECT * FROM `roadworks` WHERE MBRContains(GeomFromText('POLYGON((#{bounding_box_coords[:top_right][1]} #{bounding_box_coords[:top_right][0]}, #{bounding_box_coords[:top_left][1]} #{bounding_box_coords[:top_left][0]}, #{bounding_box_coords[:bottom_left][1]} #{bounding_box_coords[:bottom_left][0]}, #{bounding_box_coords[:bottom_right][1]} #{bounding_box_coords[:bottom_right][0]}, #{bounding_box_coords[:top_right][1]} #{bounding_box_coords[:top_right][0]}))'),
+      point);")
+
+    impact_lookup = {
+      'freeFlow' => 0.5,
+      'heavy' => 0.75,
+      'impossible' => 1
+    }
+
+    impact_sum = 0
+    rws.each do |rw|
+      impact_sum += impact_lookup[rw.impact]
+    end
+
+    impact_sum.to_f / rws.length
   end
 
-  def prediction_traffic_reports lat, long, radius
+  def traffic_reports_impact lat, long, radius
     bounding_box_coords = GeoHelpers.get_bounding_coords(lat, long, radius)
 
     reports = JourneyTime.find_by_sql("SELECT * FROM `journey_times` WHERE MBRIntersects(GeomFromText('POLYGON((#{bounding_box_coords[:top_right][1]} #{bounding_box_coords[:top_right][0]}, #{bounding_box_coords[:top_left][1]} #{bounding_box_coords[:top_left][0]}, #{bounding_box_coords[:bottom_left][1]} #{bounding_box_coords[:bottom_left][0]}, #{bounding_box_coords[:bottom_right][1]} #{bounding_box_coords[:bottom_right][0]}, #{bounding_box_coords[:top_right][1]} #{bounding_box_coords[:top_right][0]}))'),
@@ -24,14 +37,31 @@ module Helpers
       ratio_sum += report.ideal_time.to_f / report.estimated_time.to_f
     end
 
-    {
-      :total     => reports.length,
-      :ratio_avg => ratio_sum.to_f / reports.length
-    }
+    ratio_sum.to_f / reports.length
   end
 
-  def get_weather lat, long
+  def weather_impact lat, long
+    uri =  URI.parse("https://api.forecast.io/forecast/89e98468f9817b18cd7f337bd4256982/#{lat},#{long}?units=uk")
 
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    response = http.get(uri.request_uri)
+
+    json = JSON.parse(response.body)
+
+    precip_prob = json['currently']['precipProbability']
+    precib_int  = json['currently']['precipIntensity']
+    wind_speed  = json['currently']['windSpeed'].to_f / 100
+
+    (precip_prob + precib_int + wind_speed).to_f / 3
+  end
+
+  def form_prediction_desc n
+    return 'Ideal conditions for travelling - enjoy!' if n <= 0.4
+    return 'Okay to set off, however expect minor delays' if n > 0.4 and n <= 0.7
+    return 'Expect significant delays - why not set off slightly later?' if n > 0.7 and n <= 1
+    return 'Major delays! We suggest you stay put for now!' if n > 1
   end
 
 end
