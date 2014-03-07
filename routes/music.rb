2371933b1ade063af31d1329ed998d59db67284b
Alex Norton <sandig@gmail.com>
2014-03-07 01:41:37 +0000

    Music

--- routes/music.rb
+++ routes/music.rb
@@ -2,18 +2,20 @@ require 'active_support/all'
 require 'json'
 
 class WonderApp < Sinatra::Base
-  content_type :json
+  get '/music/playlist.json' do
+    content_type :json
 
-  parameters = {
-      max_liveness: 0.05
-  }
+    parameters = {
+        target_tempo: 250
+    }
 
-  uri =  URI.parse("http://developer.echonest.com/api/v4/playlist/static?api_key=IOEKRKJN4SQVXE4LO&format=json&results=20&type=genre-radio&genre=pop&genre=rock&genre=electronic&genre=indie rock&bucket=id:spotify-WW&bucket=tracks&#{parameters.to_query}")
+    url = "http://developer.echonest.com/api/v4/playlist/static?api_key=IOEKRKJN4SQVXE4LO&format=json&results=20&type=genre-radio&genre=pop&genre=rock&genre=electronic&genre=indie+rock&bucket=id:spotify-WW&bucket=tracks&#{parameters.to_query}"
 
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  response = http.get(uri.request_uri)
+    uri =  URI.parse(url)
 
-  response.body
+    http = Net::HTTP.new(uri.host, uri.port)
+    response = http.get(uri.request_uri)
+
+    response.body
+  end
 end
